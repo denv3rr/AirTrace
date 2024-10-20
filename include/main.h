9f13a6f7f1ccd304f2b7918583a51651adcec9ba
denv3rr <denver.clark@g.austincc.edu>
2024-10-20 15:06:39 -0500

    create main+h and files for object and tracker classes

---
+++ include/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include "Tracker.h"
+#include "Object.h"
+
+#endif // MAIN_H
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int main()
+{
+    {
+        std::cout << "Starting Tracking System...\n";
+
+        // Create objects
+        // Initialize tracker
+        // Select mode
+        // Set goal/target
+        // Start tracking
+    }
+
+    std::cout << "Stopped Tracking System.\n";
+    return 0;
+}
